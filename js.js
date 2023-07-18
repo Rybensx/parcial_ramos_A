03bf98fbba14c3d9dc21e45a9f003546841bda4f
Rybensx <anderson_arf@hotmail.es>
2023-07-18 10:51:22 -0600

    parcial_ramos_A creacion del endpoint2 (cambie el nombre de las ID del endpoint1 de igual forma en la const)

--- js.js
+++ js.js
@@ -1,11 +1,9 @@
-const formulario = document.getElementById('formulario');
+const formularioNombre = document.getElementById('formularioNombre');
+const formularioEpisodio = document.getElementById('formularioEpisodio');
 const main = document.querySelector('main');
 
-const consultarApi = async (e) => {
-  e.preventDefault();
-  const nombre = document.getElementById('nombre').value;
+const buscarPersonaje = async (nombre) => {
   const url = `https://rickandmortyapi.com/api/character/?name=${nombre}`;
-
   try {
     const respuesta = await fetch(url);
     const data = await respuesta.json();
@@ -33,5 +31,41 @@ const consultarApi = async (e) => {
   }
 };
 
-formulario.addEventListener('submit', consultarApi);
+const buscarEpisodio = async (episodio) => {
+  const url = `https://rickandmortyapi.com/api/episode?name=${episodio}`;
+  try {
+    const respuesta = await fetch(url);
+    const data = await respuesta.json();
+    main.innerHTML = '';
 
+    if (data.results.length > 0) {
+      data.results.forEach(episode => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+          <h2>${episode.name}</h2>
+          <p>Fecha de emisión: ${episode.air_date}</p>
+          <p>Episodio: ${episode.episode}</p>
+        `;
+        main.appendChild(article);
+      });
+    } else {
+      const mensaje = document.createTextNode('No se encontraron episodios con ese nombre.');
+      main.appendChild(mensaje);
+    }
+  } catch (error) {
+    console.error('Error en la consulta:', error);
+    main.innerHTML = '<p>Error al cargar los episodios.</p>';
+  }
+};
+
+formularioNombre.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nombre = document.getElementById('nombre').value;
+  buscarPersonaje(nombre);
+});
+
+formularioEpisodio.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const episodio = document.getElementById('episodio').value;
+  buscarEpisodio(episodio);
+});
